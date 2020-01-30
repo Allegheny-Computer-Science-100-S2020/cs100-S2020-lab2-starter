a5a966a117c037a6050db322cc7d98ce75ea7f57
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-01-30 11:24:05 -0500

    Add the starting version of the ComputeMoonDistance.

---
+++ src/main/java/labtwo/ComputeMoonDistance.java
@@ -0,0 +1,50 @@
+// TODO: Add the correct package declaration
+
+// TODO: Add the required import statement
+
+// NOTE: If you do not correctly add the above statements
+// the program will not compile correctly when you type "gradle build"
+
+/** This class has a method that computes the distance to the moon in yards.
+ * The first line displays the name of the programmer and the date. Then, the
+ * program's main method stores some values in variables and performs a
+ * computation for the number of yards to the moon. Ultimately, the class can
+ * display the number of yards to the moon.
+ *
+ * <p>Bugs: This program has no known bugs, but is missing source code
+ *
+ * @author Add Your Name Here
+ */
+
+public class ComputeMoonDistance {
+
+  /** The main entry location for ComputeMoonDistance.
+   *  The source code inside of this method will be executed when the program is
+   *  run in the command-line. The output of this program will appear in the
+   *  terminal window. The println statements make the program display output.
+   *  The program will calculate and display the distance to the moon in yards.
+   *  To learn more visit: https://spaceplace.nasa.gov/moon-distance/en/
+   *
+   * @param args The command-line arguments
+   **/
+  public static void main(String[] args) {
+    // TODO: display the name of the programmer and the date
+    // declare variables for the fixed distances
+    int milesToMoon = 238855;
+    int yardsPerMile = 1760;
+    // TODO: declare the variable that will be computed
+    // NOTE: you are computing the yards to the moon
+    // and the variable should have a suitably descriptive name
+
+    // TODO: compute the yards to the moon
+    // NOTE: you will need to write an assignment statement
+    // for the variable that you declared in the previous code
+
+    // output the declared values stored in variables
+    System.out.println("Distance to the moon in miles: " + milesToMoon);
+    System.out.println("Number of yards per mile: " + yardsPerMile);
+    // TODO: output the computed value for the number of yards to the moon
+    // NOTE: your println statement can follow the previous examples
+  }
+
+}
